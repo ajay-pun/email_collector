9de8bf8781ccc43cf6da9bca40451db6f91f3290
ajay-pun <93480088+ajay-pun@users.noreply.github.com>
2023-02-12 10:43:26 +0000

    Update email validator.py
    Prompt interface added

--- email validator.py
+++ email validator.py
@@ -5,15 +5,18 @@ from random import randint
 
 
 def run(playwright):
-    email="helloworld"
-    browser =playwright.chromium.launch(headless=False)
+    browser =playwright.chromium.launch(headless=True)
     page = browser.new_page()
     page.goto("https://accounts.google.com/signup/v2/webcreateaccount?biz=false&cc=IN&continue=https%3A%2F%2Fwww.google.com%2F&dsh=S1627445831%3A1674586118124237&flowEntry=SignUp&flowName=GlifWebSignIn&hl=en&ifkv=AWnogHeE6ezYEQseKsIsnL3Sij1w6QHgKm2F4llvOGbAMoPrX87M5Q8JfuyAdUGothHy14tW9ijl")
     page.keyboard.press("Tab")
     page.keyboard.press("Tab")
-    email_list=['helloworld','jesonpun345678','wearehero8992713','wowe2323arth','lol123']
     
-    for email in email_list:
+    while True:
+        email=input("Enter the email: ")
+        email=email.replace("@gmail.com","")
+        l=len(email)
+        if l<6 or l>35:
+            continue
         page.keyboard.type(email)
         page.keyboard.press("Shift+Tab")
         sleep(3)
